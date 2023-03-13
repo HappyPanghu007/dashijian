215ea2e06c01c151b86ecb830842392b2255caaf
happy <398925644@qq.com>
2023-03-13 23:23:57 +0800

    表单标签和变量准备

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -1,47 +1,87 @@
 <template>
-    <!-- 注册页面的整体盒子 -->
-    <div class="reg-container">
-      <!-- 注册的盒子 -->
-      <div class="reg-box">
-        <!-- 标题的盒子 -->
-        <div class="title-box"></div>
-        <!-- 注册的表单区域 -->
-      </div>
+  <!-- 注册页面的整体盒子 -->
+  <div class="reg-container">
+    <!-- 注册的盒子 -->
+    <div class="reg-box">
+      <!-- 标题的盒子 -->
+      <div class="title-box"></div>
+      <!-- 注册的表单区域 -->
+      <el-form ref="form" :model="form">
+        <el-form-item>
+          <el-input
+            placeholder="请输入用户名"
+            v-model="form.username"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入密码" v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            placeholder="请再次确认密码"
+            v-model="form.repassword"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btn-reg" type="primary" @click="registerFn"
+            >注册</el-button
+          >
+          <el-link type="info">去登录</el-link>
+        </el-form-item>
+      </el-form>
     </div>
-  </template>
+  </div>
+</template>
+
+<!-- 经验: -->
+<!-- 前端绑定数据对象属性名，可以直接给要调用的功能接口的参数名一致 -->
+<!-- 好处:我可以直接把前端对象(带着同名的属性和前端的值)发给后台 -->
 
 <script>
 export default {
-  name: 'my-register'
+  name: 'my-register',
+  data() {
+    return {
+      form: {
+        username: '', // 用户名
+        password: '', // 密码
+        repassword: '' // 确认密码
+      }
+    }
+  },
+  methods: {
+    // 注册->点击事件
+    registerFn() {}
+  }
 }
 </script>
 
-  <style lang="less" scoped>
-  .reg-container {
-    background: url('../../assets/images/login_bg.jpg') center;
-    background-size: cover;
-    height: 100%;
+<style lang="less" scoped>
+.reg-container {
+  background: url('../../assets/images/login_bg.jpg') center;
+  background-size: cover;
+  height: 100%;
 
-    .reg-box {
-      width: 400px;
-      height: 335px;
-      background-color: #fff;
-      border-radius: 3px;
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%, -50%);
-      padding: 0 30px;
-      box-sizing: border-box;
+  .reg-box {
+    width: 400px;
+    height: 335px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    padding: 0 30px;
+    box-sizing: border-box;
 
-      .title-box {
-        height: 60px;
-        background: url('../../assets/images/login_title.png') center no-repeat;
-      }
+    .title-box {
+      height: 60px;
+      background: url('../../assets/images/login_title.png') center no-repeat;
+    }
 
-      .btn-reg {
-        width: 100%;
-      }
+    .btn-reg {
+      width: 100%;
     }
   }
-  </style>
+}
+</style>
