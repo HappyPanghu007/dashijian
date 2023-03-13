636f7c86acfd40cb50d943df2e289c001ad01ddc
happy <398925644@qq.com>
2023-03-13 16:44:03 +0800

    1.初始化项目、2.配置element-ui、3.封装请求库

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button>123</el-button>
+  </div>
+</template>
+
+<script>
+// 命名导出时，导入需要加大括号
+import { registerAPI } from './api'
+export default {
+  async created () {
+    const res = await registerAPI()
+    console.log(res)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/utils/request.js
@@ -0,0 +1,11 @@
+// 基于axios封装，网络请求的函数
+import axios from 'axios'
+
+// axios.create()创建一个带配置项的自定义axios函数
+// myAxios请求的时候地址baseURL+url，然后后台请求
+
+const myAxios = axios.create({
+  baseURL: 'http://big-event-vue-api-t.itheima.net'
+})
+// 导出axios自定义的函数
+export default myAxios
