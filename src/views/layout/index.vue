77c64ec2221a37bd34d318ced0eda052b1c90c5f
happy <398925644@qq.com>
2023-03-15 20:20:27 +0800

    准备侧边栏的导航组件标签

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -36,11 +36,55 @@
     <el-container>
       <!-- 侧边栏区域 -->
       <el-aside width="200px">
+        <!-- 侧边栏的用户信息区域 -->
         <div class="user-box">
           <img :src="user_pic" alt="" v-if="user_pic" />
           <img src="../../assets/images/logo.png" alt="" v-else />
           <span>欢迎 {{ nickname || username }}</span>
         </div>
+        <!-- 侧边栏的导航菜单 -->
+        <el-menu
+          default-active="/home"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#23262E"
+          text-color="#fff"
+          active-text-color="#409EFF"
+        >
+          <el-menu-item index="/home">
+            <i class="el-icon-s-home"></i>
+            <span>首页</span>
+          </el-menu-item>
+          <el-submenu index="/topic">
+            <template slot="title">
+              <i class="el-icon-tickets"></i>
+              <span>文章管理</span>
+            </template>
+            <el-menu-item index="/topic1">
+              <i class="el-icon-s-home"></i>
+              <span>文章1</span>
+            </el-menu-item>
+            <el-menu-item index="/topic2">
+              <i class="el-icon-s-home"></i>
+              <span>文章2</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="/my">
+            <template slot="title">
+              <i class="el-icon-tickets"></i>
+              <span>个人中心</span>
+            </template>
+            <el-menu-item index="/my1">
+              <i class="el-icon-s-home"></i>
+              <span>1</span>
+            </el-menu-item>
+            <el-menu-item index="/my2">
+              <i class="el-icon-s-home"></i>
+              <span>2</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
       </el-aside>
       <el-container>
         <!-- 页面主体区域 -->
@@ -81,6 +125,12 @@ export default {
           this.$router.push('/login')
         })
         .catch((err) => err)
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
