24c5bcd509cdf44c3b3f5e1c85c582ff467f3f12
happy <398925644@qq.com>
2023-03-26 13:53:33 +0800

    打包发布-CDN在开发环境下不排除包

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,31 @@
 const { defineConfig } = require('@vue/cli-service')
+// 需要排除的包对象
+let externals = {}
+// 判断是否是生产环境
+const isProduction = process.env.NODE_ENV === 'production'
+// 如何是生产环境，需要执行以下逻辑
+if (isProduction) {
+  externals = {
+    /**
+      * externals 对象属性解析：
+      * '包名': '在项目中引入的名字'
+      * 以 element-ui 举例 我再 main.js 里是以
+      * import ELEMENT from 'element-ui'
+      * Vue.use(ELEMENT)
+      * 这样引入的，所以我的 externals 的属性值应该是 ELEMENT
+      * 一定要去main.js设置
+    */
+    echarts: 'echarts',
+    vue: 'Vue',
+    'vue-router': 'VueRouter',
+    vuex: 'Vuex',
+    axios: 'axios',
+    dayjs: 'dayjs',
+    'element-ui': 'ELEMENT',
+    'vue-quill-editor': 'VueQuillEditor',
+    'vuex-persistedstate': 'createPersistedState'
+  }
+}
 module.exports = defineConfig({
   transpileDependencies: true,
   // 影响打包时,index.html引入其他资源的前缀地址
@@ -20,15 +47,7 @@ module.exports = defineConfig({
     externals: {
       // 基本格式：
       // '包名' : '在项目中引入的名字'
-      echarts: 'echarts',
-      vue: 'Vue',
-      'vue-router': 'VueRouter',
-      vuex: 'Vuex',
-      axios: 'axios',
-      dayjs: 'dayjs',
-      'element-ui': 'ELEMENT',
-      'vue-quill-editor': 'VueQuillEditor',
-      'vuex-persistedstate': 'createPersistedState'
+      externals
     }
     // resolve: {
     //   alias: {
@@ -37,3 +56,10 @@ module.exports = defineConfig({
     // }
   }
 })
+
+// 我们虽然排除掉了第三方的包
+// 打包dist和开发环境都会使用这套配置，并排除掉第三方
+// 生产环境，打包时会以public/index.html作为模板生成dist/index.html,index.html中就有了那些cdn地址
+// 开发环境，public/index.html，也有那些第三方的cdn地址,也可以正常运行
+// 当第一次打开速度有点慢，所以开发环境呢我还是想让webpack引入本地的node_modules那些第三方包
+// process.env .NODE ENV可以根据敲击的命令不同，值也不同，能够一个代码区分不同环境
